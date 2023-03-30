249b84b4c40f71c383e37f565f7b9de1dd3a3707
Shepon Biswas <sheponbiswas01@gmail.com>
2023-03-31 03:23:00 +0600

    JSON file added, fetch the data, and show on blogs page

--- src/components/BlogCard/BlogCard.jsx
+++ src/components/BlogCard/BlogCard.jsx
@@ -4,37 +4,42 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from "react";
 
 
-const BlogCard = () => {
+const BlogCard = (props) => {
+    // console.log(props)
+    const {author_name, author_image, blog_title, cover_image, publish_date, read_time} = props.blog;
+    // console.log(author_name)
+
+
   return (
     <>
     <div className='w-8/12'>
       <div className='w-full'>
         <img
         className='rounded-lg w-full'
-          src="https://images.pexels.com/photos/546819/pexels-photo-546819.jpeg?auto=compress&cs=tinysrgb&w=500&h=750&dpr=1"
-          alt=""
+          src={cover_image}
+          alt="cover-images"
         />
         <div>
           <div className='flex justify-between items-center px-1 my-3'>
             <div className='flex justify-between'>
               <img
                 className="w-12 h-12 rounded-full"
-                src="https://images.pexels.com/photos/3366753/pexels-photo-3366753.jpeg?auto=compress&cs=tinysrgb&w=500&h=750&dpr=1"
-                alt=""
+                src={author_image}
+                alt="author-image"
               />
               <div className='ms-2'>
-              <h4 className='font-bold text-xl'>Name</h4>
-              <p className='text-gray-400'>20th March 2023</p>
+              <h4 className='font-bold text-xl'>{author_name}</h4>
+              <p className='text-gray-400'>{publish_date}</p>
               </div>
             </div>
             <div className='flex justify-between gap-2'>
-                <h4>05 Min to read</h4>
+                <h4>{read_time} Min to read</h4>
                 <button><FontAwesomeIcon icon={faBookmark} /></button>
             </div>
           </div>
         </div>
         <div className='p-2'>
-            <h1 className='font-bold text-3xl'>That was fast! Microsoft slips ads into AI-powered Bing Chat</h1>
+            <h1 className='font-bold text-3xl'>{blog_title}</h1>
             <p className='text-gray-500 my-4 cursor-pointer'>#programming #AIupdates</p>
 
             <button className=' underline underline-offset-4 text-orange-500 font-bold mb-4'>
